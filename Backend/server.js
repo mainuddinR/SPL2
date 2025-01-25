263af09780d90b0d7380902e393a4f544cc72a01
mainuddinR <bsse1304@iit.du.ac.bd>
2025-01-25 13:18:21 +0600

    Authentication backend part

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+import  express  from "express";
+import cors from "cors"
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
+
+
+
+//app config
+const app = express()
+const port = 4000 //5173
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/user",userRouter)
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
+//mongodb+srv://mainuddin:11223344@cluster0.aa9el.mongodb.net/?
