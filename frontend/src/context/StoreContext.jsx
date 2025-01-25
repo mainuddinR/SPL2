6522d0917c21232940f2a8c3ab7ef3fa76e35df0
mainuddinR <bsse1304@iit.du.ac.bd>
2025-01-25 19:36:24 +0600

    complete Authentication

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,6 +7,9 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
+    const url = "http://localhost:4000";
+    const [token,setToken] = useState("");
+
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
@@ -35,6 +38,11 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    useEffect(()=>{
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"));
+        }
+    },[])
 
     const contextValue = {
         food_list,
@@ -42,7 +50,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
