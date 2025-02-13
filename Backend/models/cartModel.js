b86c9029fe3b2807323f3f83257e17b28db4046c
mainuddinR <bsse1304@iit.du.ac.bd>
2025-02-13 22:29:00 +0600

    cart class create

--- Backend/models/cartModel.js
+++ Backend/models/cartModel.js
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
-    itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true },
-    quantity: { type: Number, required: true }
-  });
+// const itemSchema = new mongoose.Schema({
+//     itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'item', required: true },
+//     quantity: { type: Number, required: true ,min:1}
+//   });
   
   const cartSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    items: [itemSchema],
-    totalPrice: { type: Number, default: 0 },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    items: [
+      {
+        itemId: { type: mongoose.Schema.Types.ObjectId, ref: "item", required: true },
+        quantity: { type: Number, required: true , min:1 },
+      },
+    ],
+    totalPrice: { type: Number,required:true, default: 0 },
     createdAt: { type: Date, default: Date.now }
   });
   
-  cartSchema.methods.calculateTotal = function() {
-    this.totalPrice = this.items.reduce((acc, item) => acc + item.quantity, 0);
-    return this.totalPrice;
-  };
+  // cartSchema.methods.calculateTotal = function() {
+  //   this.totalPrice = this.items.reduce((acc, item) => acc + item.quantity, 0);
+  //   return this.totalPrice;
+  // };
   
-  const cartModel = mongoose.model('Cart', cartSchema);
+  const cartModel = mongoose.models.Cart|| mongoose.model('Cart', cartSchema);
   
   export default cartModel;
