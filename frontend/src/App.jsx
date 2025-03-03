70c8a5810d7e976f4faafdc1a5f98b4ec386a878
Jihad011 <jihadroberta@gmail.com>
2025-03-04 01:47:14 +0600

    verify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Profile from './pages/profile/profile'
+import Verify from './pages/Verify/Verify'
 
 const App = () => {
 
@@ -22,6 +23,7 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
           <Route path='/profile' element={<Profile/>}/>
+          <Route path='/verify' element={<Verify/>}/>
         </Routes>
       </div>
       <Footer />
---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Verify.css'
+const Verify = () => {
+  return (
+    <div>
+      <h1>Abbb</h1>
+    </div>
+  )
+}
+
+export default Verify
